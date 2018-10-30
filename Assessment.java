328aa6ee660e137315da13a946d599798d29f56d
jiale <tanjiale1029@gmail.com>
2018-10-30 16:14:44 +0800

    added Assessment

---
+++ Assessment.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Assessment{
+
+    private String assessmentName;
+    private Double weightage;
+
+    public String getAssessmentName(){
+        return this.assessmentName;
+    }
+
+    public Double getWeightage(){
+        return this.weightage;
+    }
+
+    public void setAssessmentName(String assessmentName){
+        this.assessmentName = assessmentName;
+    }
+
+    public void setWeightage(Double weightage){
+        this.weightage = weightage;
+    }
+
+    
+}
