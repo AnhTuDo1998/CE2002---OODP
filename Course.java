6b11d2a20ebe825781b187299af57908fa8fb388
James Tan <jamestancheemin98@gmail.com>
2018-10-30 12:18:43 +0800

    added course

--- Course.java
+++ Course.java
@@ -1,5 +1,19 @@
 import java.util.*;
 
 public class Course{
+    private ArrayList<Index> indexList = new ArrayList<Index>();
+    private String courseName;
+    private String courseCode;
+    private int AU;
+    private String courseCoordinator;
+    private ArrayList<Assessment> results = new ArrayList<Assessment>();
 
+    public String getCourseName(){
+        return this.courseName;
+    }
+
+    public String getCourseCode(){
+        return this.courseCode;
+    }
+    
 }
