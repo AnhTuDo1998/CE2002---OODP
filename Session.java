4df5658c4883db7c62b754a146163c312300feff
jiale <tanjiale1029@gmail.com>
2018-10-30 17:45:17 +0800

    added some functions

--- Session.java
+++ Session.java
@@ -8,6 +8,16 @@ public class Session{
     private int numberRegistered;
     private ArrayList<Student> studentList = new ArrayList<Student>();
 
+    //create a session
+    public Session(String type, String group, String dateTime, String tutorName, int maxCapacity, int numberRegistered){
+        this.type = type;
+        this.group = group;
+        this.dateTime = dateTime;
+        this.tutorName = tutorName;
+        this.maxCapacity = maxCapacity;
+        this.numberRegistered = numberRegistered;
+    }
+
     public String getType(){ //return type of session e.g. LEC, LAB, TUT
         return this.type;
     }
@@ -16,13 +26,23 @@ public class Session{
         return this.group;
     }
 
-    public String dateTime(){
+    public String dateTime(){  //return the date and time for the session
         return this.dateTime;
     }
 
-    public String tutorName(){
+    public String tutorName(){ //return the name of tutor
         return this.tutorName;
     }
 
-    
+    public int maxCapacity(){
+        return this.maxCapacity;
+    }
+
+    public int numberRegistered(){
+        return this.numberRegistered;
+    }
+
+    public addStudent(){ //add a student into an existing group
+        
+    }
 }
