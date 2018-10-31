8a1a7a296796d70181c446817571a6711b9e3172
James Tan <jamestancheemin98@gmail.com>
2018-10-31 11:49:46 +0800

    added location

--- Session.java
+++ Session.java
@@ -3,20 +3,21 @@ public class Session{
     private String type; //lec OR tut or lab
     private String group;
     private String dayTime;
-    
+    private String location;
     private String tutorName;
     private int maxCapacity;
     private int numberRegistered;
     private ArrayList<Student> studentList = new ArrayList<Student>();
-
+    
     //create a session
-    public Session(String type, String group, String dateTime, String tutorName, int maxCapacity, int numberRegistered){
+    public Session(String type, String group, String dayTime, String location, String tutorName, int maxCapacity, int numberRegistered){
         this.type = type;
         this.group = group;
-        this.dateTime = dateTime;
+        this.dayTime = dayTime;
         this.tutorName = tutorName;
         this.maxCapacity = maxCapacity;
         this.numberRegistered = numberRegistered;
+        this.location = location;
     }
 
     public String getType(){ //return type of session e.g. LEC, LAB, TUT
@@ -27,8 +28,8 @@ public class Session{
         return this.group;
     }
 
-    public String dateTime(){  //return the date and time for the session
-        return this.dateTime;
+    public String dayTime(){  //return the date and time for the session
+        return this.dayTime;
     }
 
     public String tutorName(){ //return the name of tutor
@@ -39,6 +40,10 @@ public class Session{
         return this.maxCapacity;
     }
 
+    public String location(){
+        return this.location;
+    }
+    
     public int numberRegistered(){
         return this.numberRegistered;
     }
@@ -52,6 +57,6 @@ public class Session{
             return 1; //student added
         }
     }
-
+    
 
 }
