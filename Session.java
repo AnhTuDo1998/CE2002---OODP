900108a2a52700d4159155b4b6a51a4345baf1dc
James Tan <jamestancheemin98@gmail.com>
2018-10-31 11:37:33 +0800

    changed day from date

--- Session.java
+++ Session.java
@@ -2,7 +2,8 @@ import java.util.*;
 public class Session{
     private String type; //lec OR tut or lab
     private String group;
-    private String dateTime;
+    private String dayTime;
+    
     private String tutorName;
     private int maxCapacity;
     private int numberRegistered;
