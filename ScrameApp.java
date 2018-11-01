147ecaf7b132d72ab08a647329831591f35b81de
Do Anh Tu <32509869+AnhTuDo1998@users.noreply.github.com>
2018-11-01 15:53:01 +0800

    Added main interface

---
+++ ScrameApp.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class ScrameApp{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        StudentManager studMg = new StudentManager();
+        CourseManager courseMg = new CourseManager();
+        int choice;
+        boolean cont = true;
+        while (cont){
+            System.out.println("Select a function: ");
+            System.out.println("1. Add a student ");
+            System.out.println("2. Add a course");
+            System.out.println("3. Register student for a course");
+            System.out.println("4. Check available slot in a class");
+            System.out.println("5. Print student list");
+            System.out.println("6. Enter course's assessment weightage");
+            System.out.println("7. Enter coursework mark");
+            System.out.println("8. Enter exam mark");
+            System.out.println("9. Print course statistics");
+            System.out.println("10. Print student transcript");
+            System.out.println("11. Exit");
+            choice = sc.nextInt();
+            switch(choice){
+                case 1: //add a student
+                    studMg.addStudent();
+                    break;
+                case 2: //add a course
+                    courseMg.addCourse();
+                    break;
+                case 3: //register student for a course
+                    //studMg.regStudentToCourse();
+                    break;
+                case 4: //Check available slots in a class
+                    break;
+                case 5: //print student list
+                    break;
+                case 6: //enter course assessment weightage
+                    break;
+                case 7: //enter coursework mark
+                    break;
+                case 8: //enter exam mark
+                    break;
+                case 9: //print course stats
+                    break;
+                case 10: //print student transcript
+                    break;
+                case 11: //exit
+                    cont = false;
+                    System.out.println("Exit....");
+                    break;
+                default:
+                    System.out.println("Invalid choice, please try again!");
+            }
+        }
+        sc.close();
+
+    }
+}
