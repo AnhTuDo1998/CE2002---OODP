7241076d15c4f199045e006d35ac1d8c278e6228
Do Anh Tu <32509869+AnhTuDo1998@users.noreply.github.com>
2018-11-10 17:38:26 +0800

    Better javadoc documentation for Session class

--- Session.java
+++ Session.java
@@ -15,14 +15,14 @@ public class Session implements Serializable{
     private ArrayList<Student> studentList = new ArrayList<Student>();
     
     /**
-     * Constructor for session object, instantiating the above attributes
-     * @param type String type of session (LEC, TUT or LAB for Lecture, Tutorial and Lab)
-     * @param group String group ID of the session (CE1, SE3, etc)
-     * @param dayTime String timing for the session, following "Day of the week HH:MM - HH:MM" format
-     * @param location String describing the venue for the session
-     * @param tutorName String containing tutor name for that session
-     * @param maxCapacity int vacancy of the session (number of students it can accomodate at the moment)
-     * @param numberRegistered int describing number of student currently registered
+     * Constructor for Session object, instantiating the below attributes
+     * @param type String type of session (LEC, TUT or LAB for Lecture, Tutorial and Lab).
+     * @param group String group ID of the session (CE1, SE3, etc).
+     * @param dayTime String timing for the session, following "Day of the week HH:MM - HH:MM" format.
+     * @param location String describing the venue for the session.
+     * @param tutorName String containing tutor name for that session.
+     * @param maxCapacity int vacancy of the session (number of students it can accomodate at the moment).
+     * @param numberRegistered int describing number of student currently registered.
      */
     public Session(String type, String group, String dayTime, String location, String tutorName, int maxCapacity, int numberRegistered){
         this.type = type;
@@ -35,77 +35,88 @@ public class Session implements Serializable{
     }
 
     /**
-     * @param type String type of session (LEC, TUT or LAB for Lecture, Tutorial and Lab)
+     * A method to set the value of the calling Session object.
+     * @param type String type of session (LEC, TUT or LAB for Lecture, Tutorial and Lab).
      */
     public void setType(String type){
         this.type = type;
     }
 
     /**
-     * @param group String group ID of the session (CE1, SE3, etc)
+     * A method to set the group ID of the calling Session object.
+     * @param group String group ID of the session (CE1, SE3, etc).
      */
     public void setGroup(String group){
         this.group = group;
     }
 
     /**
-     * @param dayTime String timing for the session, following "Day of the week HH:MM - HH:MM" format
-     * e.g. Fri 14:30 - 16:30
+     * A method to set the timings of the calling Session object.
+     * @param dayTime String timing for the session, following "Day of the week HH:MM - HH:MM" format.
+     * For instance: Fri 14:30 - 16:30 
      */
     public void setDayTime(String dayTime){
         this.dayTime = dayTime;
     }
     
     /**
-     * @param tutorName String containing tutor name for that session
+     * A method to set the name of the tutor in charge of the calling session.
+     * @param tutorName String containing tutor name for that session.
      */
     public void setTutorName(String tutorName){
         this.tutorName = tutorName;
     }
 
     /**
-     * @param maxCapacity int describing max number of student can be accomodated by the session
+     * A method to set the max number of students the calling Session object can accomodate at all time.
+     * @param maxCapacity int max number of student can be accomodated by the session.
      */
     public void setMaxCapacity(int maxCapacity){
         this.maxCapacity = maxCapacity;
     }
 
     /**
-     * @param numberRegistered int describing number of student currently registered
+     * A method to set the number of students being registered under the calling Session object at the moment
+     * @param numberRegistered int number of student currently registered.
      */
     public void setNumberRegistered(int numberRegistered){
         this.numberRegistered = numberRegistered;
     }
 
     /**
-     * @param location String describing the venue for the session
+     * A method to set the venue where the calling Session object is conducted.
+     * @param location String describing the venue for the session.
      */
     public void setLocation (String location){
         this.location = location;
     }
 
     /**
-     * @return String type of session e.g. LEC, LAB, TUT.
+     * A method to return the type of calling Session object, in this application we limited to Lecture, Laboratory or Tutorial.
+     * @return String type of session, in abbrevation - LEC, LAB, TUT.
      */
     public String getType(){
         return this.type;
     }
 
     /**
-     * @return String group ID of the session
+     * A method to return the group ID (for instance CE1) of the calling Session object.
+     * @return String group ID of the session.
      */
     public String getGroup(){ 
         return this.group;
     }
 
     /** 
-     * @return String day and time of the session
+     * A method to return the timing for the calling Session object.
+     * @return String day and time of the session.
      */
     public String getDayTime(){  
         return this.dayTime;
     }
 
     /**
+     * A method to return the name of tutor being in-charge of the calling Session object.
      * @return String name of the tutor for the session
      */
     public String getTutorName(){ 
@@ -113,6 +124,7 @@ public class Session implements Serializable{
     }
 
     /**
+     * A method to return the max number of student the calling Session object can accomodate at all time.
      * @return int max number of students can be accomodated
      */
     public int getMaxCapacity(){
@@ -120,6 +132,7 @@ public class Session implements Serializable{
     }
 
     /**
+     * A method to return the venue where the calling Session object is being conducted.
      * @return String venue of the session
      */
     public String getLocation(){
@@ -127,12 +140,15 @@ public class Session implements Serializable{
     }
     
     /**
-     * @return int vacancy of the session (number of students it can accomodate at the moment)
+     * A method to calculate and return the current vacancy for the calling Session object
+     * @return int vacancy of the session , which is number of students it can accomodate at the moment, determined by the difference 
+     * between max number of student can be accomodated by the session and the number of students currently registered under it.
      */
     public int getVacancy(){
         return this.maxCapacity - this.numberRegistered;
     }
     /**
+     * A method to return the number of students currently registered under the calling Session object.
      * @return int number of student currently registered
      */
     public int getNumberRegistered(){
@@ -141,9 +157,10 @@ public class Session implements Serializable{
 
     /**
      * A method to deregister student from the calling session. 
-     * @param student Student object to be deregistered (removed) from the calling session 
-     * @return false if the process failed
-     * @return true if the student is successfully removed from the student list of the session
+     * @param student Student object to be deregistered (removed) from the calling session. 
+     * @return false if the process failed.
+     * @return true if the student is successfully removed from the student list of the session.
+     * @see ArrayList<Student> 
      */
     public boolean deregisterStudent(Student student){
         if(student != null){
@@ -158,6 +175,7 @@ public class Session implements Serializable{
      * @return int -1 if the session is full (vacancy = 0)
      * @return int -2 if the student being added is already inside the session
      * @return int 0 if the adding of student under the session is SUCCESS
+     * @see ArrayList<Student> 
      */
     public int addStudent(Student student){ //add a student into an existing group
         if(this.maxCapacity == this.numberRegistered){
@@ -173,7 +191,8 @@ public class Session implements Serializable{
     }
     
     /**
-     * Print out the list of student currently registered in this session
+     * A method to print out the list of student currently registered in this session. 
+     * @see ArrayList<Student> 
      */
     public void printSessionStudent(){
         int i;
@@ -184,14 +203,16 @@ public class Session implements Serializable{
 
     /**
      * Overided toString() method to generate a string with information about the session
+     * Used in printing out the Session object for more effeciency.
      */
     public String toString(){
         return (this.type + " | Group ID: " + this.group + " | Timings:  " + this.dayTime + "  | Venue: " + this.location + " | TA Name: " + getTutorName());
     }
 
     /**
-     * A getter method to access the array of student currently registered in this session
+     * A method to access the array of student currently registered in this session
      * @return studentList ArrayList<Student> an array of students currently registered
+     * @see ArrayList<Student> 
      */
     public ArrayList<Student> getStudentRegistered(){
         return this.studentList;
