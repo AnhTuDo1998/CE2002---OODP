a8445d8d595eb5cf36d3688033da594a19e8fed6
James Tan <jamestancheemin98@gmail.com>
2018-11-12 12:13:59 +0800

    added some javadocs

--- Database.java
+++ Database.java
@@ -1,18 +1,39 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ * This is an entity class that mainly stores all data related to this application.
+ */
 public class Database implements Serializable{
     private ArrayList<Student> studentCatalog= new ArrayList<Student>();
     private ArrayList<Course> courseCatalog = new ArrayList<Course>();
 
+    /**
+	 * A method to get the ArrayList of {@link Student} stored in this application.
+	 * @return ArrayList<Student> all students registered in this application.
+     * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html">ArrayList</a>
+	 */
     public ArrayList<Student> getStudentCatalog(){
         return this.studentCatalog;
     }
 
+    /**
+     * A method to get the ArrayList of {@link Course} stored in this application.
+     * @return ArrayList<Course> all courses registered in this application.
+     * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html">ArrayList</a>
+     */
     public ArrayList<Course> getCourseCatalog(){
         return this.courseCatalog;
     }
 
+    /**
+     * A method to add a {@link Student} into the database of this application.
+     * @param String name of the new course.
+     * @param String course code of the new course.
+     * @param int academic units allocated to this course.
+     * @param String name of the course coordinator.
+     * @return Course the newly created course.
+     */
     public Course addCourse(String courseName, String courseCode, int AU, String courseCoordinator){
 
         Course created = null;
@@ -23,13 +44,34 @@ public class Database implements Serializable{
         return created;
     }
     
-    
+    /**
+     * A method to add a {@link Student} into the database of this application.
+     * @param String name of the new student.
+     * @param String matriculation number of the new student.
+     * @param char gender of the new student.
+     * @param String name of the school of the new student.
+     * @param int current academic year of the new student.
+     * @return Student the newly created student.
+     */
     public Student addStudent(String studentName,String  matricNumber,char gender,String school,int acadYear){
         Student student = new Student(studentName, matricNumber, gender, school, acadYear);
         studentCatalog.add(student);
         return student;
     }
 
+    /**
+     * A method to remove an existing student from the database of this application.
+     * <p>
+     * This method will also remove all students from its registered sessions 
+     * by iterating through its registered {@link Course} and all {@link Session} under
+     * the courses, results of the student will be erased as well.
+     * </p>
+     * @param Student the student object to be removed.
+     * @see Course#getAssessment
+     * @see Session#getAllSession
+     * @see Session#getStudentsRegistered
+     * @see Assessment#removeAssessmentResult
+     */
     public void removeStudent(Student student){
         if(studentCatalog.remove(student)){
             for(int i = 0; i < courseCatalog.size(); i++){
@@ -55,9 +97,16 @@ public class Database implements Serializable{
         }
     }
 
+    /**
+     * A method to remove an existing {@link Course} from the database of this application.
+     * <p>
+     * All students that are currently registered to this course will be deregistered.
+     * @param Course the course to be removed.
+     * @see Student#deregisterCourse
+     */
     public boolean removeCourse(Course course){
     
-        //return deleted course, null of none
+        //return deleted course, null ff none
         if (course != null){
             if(courseCatalog.remove(course)){
                 for(int i = 0 ; i < studentCatalog.size(); i++){
@@ -69,6 +118,9 @@ public class Database implements Serializable{
         return false;
     }
 
+    /**
+     * A method to print out all existing {@link Course} in the database of this application.
+     */
     public void printCourseCatalog(){
         int i;
         System.out.println("Course in current Catalog: ");
@@ -81,7 +133,9 @@ public class Database implements Serializable{
         System.out.println();
     }
 
-    
+    /**
+     * A method to print out all existing {@link Student} in the database of this application.
+     */
     public void printStudentCatalog(){
         int i;
         System.out.println("All students in record: ");
@@ -94,6 +148,11 @@ public class Database implements Serializable{
         System.out.println();
     }
 
+    /**
+     * A method to obtain the object {@link Course} stored in the database.
+     * @param String course code of the target course.
+     * @return Course target course object, null if not in database.
+     */
     public Course getCourse(String courseCode){
         for(int i = 0; i < courseCatalog.size(); i++){
             if(courseCatalog.get(i).getCourseCode().equals(courseCode)) return courseCatalog.get(i);
@@ -101,6 +160,11 @@ public class Database implements Serializable{
         return null;
     }
 
+    /**
+     * A method to obtain the object {@link Student} stored in this database.
+     * @param String matriculation number of the target student.
+     * @return Student target student object, null if not in database.
+     */
     public Student getStudent(String matricNumber){
         for(int i = 0; i < studentCatalog.size(); i++){
             if(studentCatalog.get(i).getMatricNumber().equals(matricNumber)) return studentCatalog.get(i);
