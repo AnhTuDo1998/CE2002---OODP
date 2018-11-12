e4da026024207cda05889567f02f0e150673c6db
Do Anh Tu <32509869+AnhTuDo1998@users.noreply.github.com>
2018-11-12 18:33:55 +0800

    Yeaboi

--- Course.java
+++ Course.java
@@ -1,6 +1,8 @@
 import java.util.*;
 import java.io.*;
-
+/**
+ * 
+ */
 public class Course implements Serializable{
     //Record of Session (Tut, Lab and Lecture) under a course.
     private ArrayList<Session> indexList = new ArrayList<Session>();
@@ -58,11 +60,9 @@ public class Course implements Serializable{
         return this.courseCoordinator;
     }
     /**
-     * A method to add in new Session under the calling Course object. 
-     * @return boolean false if the new Session object is not added,
-     * return true if the new Session object is added
-     * @see Session for Session object usage.
-     * @see #indexList
+     * A method to add in new {@link Session} under the calling Course object (making use of ArrayList for storage). 
+     * @return boolean false if the new Session object is not added, true if the new Session object is added
+     * @see <a href = https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html"> HashMap </a>
      */
     public boolean addSession() throws StringIndexOutOfBoundsException{
         boolean success = false;
@@ -100,7 +100,13 @@ public class Course implements Serializable{
         return success;
     }
 
-    //Modifying Session
+    /**
+     * A method to modify information about existing {@link Session}. 
+     * <p> The method parse in the Session object need to be modified, following by accepting inputs from users to change the wanted
+     * field of information for that session.
+     * @param session The Session that need modification (for example changing any of the following: Type, Group ID, Timing, ...)
+     * @return boolean true if the modification is successful and false if otherwise.
+     */
     public boolean modifySession(Session session){
         boolean success = false;
         char conti = 'Y';
@@ -174,7 +180,11 @@ public class Course implements Serializable{
         return success;         
     }
 
-    //print session catalogue
+    /**
+     * An utility method to print out all {@link Session} existing under the calling Course object.
+     * <p> This method traverse the ArrayList of sessions stored in the Course and print out the Session objects visited.
+     * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html">ArrayList</a>
+     */
     public void printIndexList(){
         int i;
 
@@ -184,6 +194,12 @@ public class Course implements Serializable{
         }
     }
 
+    /**
+     * A method to return a {@link Session} under the calling Course object
+     * @param group the group ID of the Session of interest
+     * @param type the type of Session of interest (LEC, TUT or LAB)
+     * @return Session the session object of interest.
+     */
     public Session getSession(String group, String type){
         int i;
         Session obtained = null;
@@ -198,15 +214,25 @@ public class Course implements Serializable{
     }
 
     /**
-     * A method to set the Assessment information of the calling Course object.
+     * A method to store the {@link Assessment} information under the calling Course object.
+     * <p> Making use of the ArrayList of Assessment by adding the parsed in Assessment object into it.
      * @param assessment Assessment component of this Course object. 
-     * @return
+     * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html">ArrayList</a>
      */
     public void setAssessment(Assessment assessment){
         results.add(assessment);
     }
 
-    //return 0 if added successfully, -1 if full, -2 if student is inside -3 if group does not exist
+    /**
+     * A method to register {@link Student} into {@link Session} stored in this Course.
+     * <p> This method take in Student object, the group and type of Session needed and then traverse the array of Session objects of the calling Course object and add the Student in by addStudent method from Session class.
+     * @param student the Student we need to register
+     * @param group the group ID of the Session
+     * @param type the type of the Session (LEC, TUT or LAB)
+     * @return int 0 if added successfully, -1 if full, -2 if student is inside -3 if group does not exist
+     * @see Session#addStudent(Student)
+     * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html">ArrayList</a>
+     */
     public int registerStudent(Student student, String group, String type){
         int result = -3;
         for(int i = 0; i < indexList.size();  i++){
@@ -218,6 +244,23 @@ public class Course implements Serializable{
         return result;
     }
 
+    /**
+     * A method to remove {@link Student} from all {@link Session} and erase his/her {@link Assessment} data under the calling Course object.
+     * <p> This is done by first traversing the ArrayList of Session, getting the ArrayList of Student objects registered under each
+     * Session. Then we search for the matching Student in the current ArrayList of Student. 
+     * <p>If there is a valid, matching entry of Student, 
+     * we remove that Student and update the number of Student registered under that Session. 
+     * Else, continue until no more unvisited Student under the current Session.
+     * <p> We then remove all the Assessment results of the Student by traversing and removing from the array list of hashmap that stored all 
+     * result for the student for the course.
+     * @param student
+     * @return int number of Session that the Student were removed from for debugging purpose.
+     * @see Session#getStudentRegistered()
+     * @see Session#setNumberRegistered(int)
+     * @see Assessment#removeAssessmentResult(Student)
+     * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html">ArrayList</a>
+     * @see <a href = https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html"> HashMap </a>
+     */
     public int deregisterStudent(Student student){
         int sessionCount = 0;
         for(int i = 0; i < indexList.size(); i++){
@@ -237,6 +280,11 @@ public class Course implements Serializable{
         return sessionCount;
     }
 
+    /**
+     * A method to print out all {@link Session} under the calling Course object
+     * <p> Traverse the ArrayList of Session objects and print out the information needed.
+     * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html">ArrayList</a>
+     */
     public void printSessions(){
         for (int i = 0; i < indexList.size(); i++){
             System.out.println(indexList.get(i) + " | Vacancy: " 
@@ -244,10 +292,19 @@ public class Course implements Serializable{
         }
     }
 
+    /**
+     * A method to return the ArrayList of {@link Assessment} objects under the calling Course
+     * @return ArrayList<Assessment> the array storiing the Assessment objects (each object correspond to 1 component such as Final exam, quiz, etc)
+     * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html">ArrayList</a>
+     */
     public ArrayList<Assessment> getAssessment(){
         return this.results;
     }
 
+    /**
+     * A method to return String containing the information of the Course object. This can be use for printing out the object.
+     * @return String the String containing information of the Course object, formatted.
+     */
     public String toString(){
         String codeFormat = "|| %1$6s :";
         String nameFormat = " %2$-30s ";
@@ -257,10 +314,19 @@ public class Course implements Serializable{
         return String.format(format, this.courseCode, this.courseName, "Coordinator: " + this.courseCoordinator, "AU: " + this.AU);
     }
 
+    /**
+     * A method to return an ArrayList storing all {@link Session} currently under the calling Course object
+     * @return an ArrayList of all Session associated with the calling Course.
+     * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html">ArrayList</a>
+     */
     public ArrayList<Session> getAllSession(){
         return this.indexList;
     }
-
+    /**
+     * A method to validate if {@link Student} is registered under the calling Course or not.
+     * @param student the object of Student who we are interest to know his status under this course
+     * @return boolean true if the Student is under the Course, else false is returned.
+     */
     public boolean studentRegistered(Student student){
         for(int i = 0; i < indexList.size(); i++){
             if(indexList.get(i).getStudentRegistered().contains(student)) return true;
@@ -268,10 +334,22 @@ public class Course implements Serializable{
         return false;
     }
 
+    /**
+     * A method to clear all {@link Assessment} objects/components under the Course. 
+     * <p> This is done my clearing the ArrayList of Assessment object under the Course.
+     * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html">ArrayList</a>
+     */
     public void clearAssessments(){
         results.clear();
     }
 
+    /**
+     * A method to store result of {@link Student} for an {@link Assessment} component under this Course.
+     * @param assessment the component that we need to store the result in (for instance finals, coursework, etc)
+     * @param student the student who we need to key in result.
+     * @param marks the grade of Student
+     * @see Assessment#storeAssessmentResult(Student, double)
+     */
     public void enterResults(Assessment assessment, Student student, double marks){
         assessment.storeAssessmentResult(student, marks);
     }
