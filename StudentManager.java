0f4afe49d24df5b6c155e65953a7519362cd4116
James Tan <jamestancheemin98@gmail.com>
2018-11-12 22:57:29 +0800

    added CGPA in student transcript

--- StudentManager.java
+++ StudentManager.java
@@ -30,10 +30,12 @@ public class StudentManager{
     public void printTranscript(Student student){
         ScrameApp.printSpaces();
         System.out.println(student);
-        System.out.println("============================== TRANSCRIPT ===============================");
+        System.out.println("==================================== TRANSCRIPT ======================================");
         ArrayList<Course> courseRegistered = student.getCourseRegistered();
         ArrayList<Assessment> results = null;
         double totalResults = 0;
+        double totalGPA = 0;
+        int totalAU = 0;
         String grade = "F";
         for(int i = 0; i < courseRegistered.size(); i++){
             totalResults = 0;
@@ -43,15 +45,18 @@ public class StudentManager{
                 System.out.printf("%1$35s Marks : %2$1s (%3$2s%%)\n", results.get(j).getAssessmentName(), results.get(j).retrieveAssessmentResult(student), results.get(j).getWeightage());
                 totalResults += results.get(j).retrieveAssessmentResult(student) * results.get(j).getWeightage() / 100;
             }
-            System.out.println("============================= Final Grade :" + marksToGrade(totalResults) + " ============================");
-            System.out.println("=========================================================================");
+            System.out.println("======================= Final Grade: " + marksToGrade(totalResults) + " ======= GPA: " + marksToGPA(totalResults) + " =============================");
+            System.out.println("======================================================================================");
             System.out.println();
+            totalAU += courseRegistered.get(i).getAU();
+            totalGPA += courseRegistered.get(i).getAU()*marksToGPA(totalResults);
         }
+        System.out.println("====================================== CGPA : " + totalGPA/totalAU + " ===================================");
     }
 
     /**
-     * A utility method to convert number mark to letter grade
-     * @param double the numberic marks (for instance 75) to be converted
+     * A utility method to convert number mark to letter grade.
+     * @param double the numeric marks to be converted
      * @return String the letter grade of the numeric mark being queried.
      */
     public String marksToGrade(double marks){
@@ -65,6 +70,25 @@ public class StudentManager{
         if(marks > 40) return "C";
         if(marks > 30) return "D";
         return "F";
+    }
+    
+    /**
+     * A utility method to convert number mark to numerical grade-point average.
+     * @param marks the numeric marks to be converted.
+     * @return the GPA of the subject.
+     */
+    public double marksToGPA(double marks){
+        if(marks > 90) return 5.00;
+        if(marks > 80) return 5.00;
+        if(marks > 75) return 4.50;
+        if(marks > 70) return 4.00;
+        if(marks > 60) return 3.50;
+        if(marks > 50) return 3.00;
+        if(marks > 45) return 2.50;
+        if(marks > 40) return 2.00;
+        if(marks > 30) return 1.50;
+        if(marks > 20) return 1.00;
+        return 0.00;
     }
 
     /**
